ad6bd9099f6aa3e1ef2721b8a401af062f59e6b0
a.shevtsov <a.shevtsov@paysend.com>
2021-07-08 13:48:08 +0300

    Test fixed image

--- esphome/components/esp32cam_web_stream_simple/base_image_web_stream.cpp
+++ esphome/components/esp32cam_web_stream_simple/base_image_web_stream.cpp
@@ -241,7 +241,7 @@ class BaseImageWebStreamHandler : public AsyncWebHandler {
 
                 size_t i = len - this->webChunkSent_;
                 //                size_t m = maxLen;
-                size_t m1 = 1024 * 10;
+                size_t m1 = 1024;
                 const size_t m = std::min(maxLen, m1);
 
                 if (i <= 0) {
